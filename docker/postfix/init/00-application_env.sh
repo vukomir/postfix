fb364b716d47eab465df9a9e3e2675493f089819
Vucomir Ianculov <vucomir.ianculov@azets.com>
2018-02-25 10:28:07 -0800

    first commit

---
+++ docker/postfix/init/00-application_env.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# Set up host name
+if [ ! -z "$HOSTNAME" ]; then
+	postconf -e myhostname="$HOSTNAME"
+else
+	postconf -# myhostname
+fi
+
+# Set up a relay host, if needed
+if [ ! -z "$RELAYHOST" ]; then
+	echo -n "- Forwarding all emails to $RELAYHOST"
+	postconf -e relayhost=$RELAYHOST
+
+	if [ -n "$RELAYHOST_USERNAME" ] && [ -n "$RELAYHOST_PASSWORD" ]; then
+		echo " using username $RELAYHOST_USERNAME."
+		echo "$RELAYHOST $RELAYHOST_USERNAME:$RELAYHOST_PASSWORD" >> /etc/postfix/sasl_passwd
+		postmap hash:/etc/postfix/sasl_passwd
+		postconf -e "smtp_sasl_auth_enable=yes"
+		postconf -e "smtp_sasl_password_maps=hash:/etc/postfix/sasl_passwd"
+		postconf -e "smtp_sasl_security_options=noanonymous"
+	else
+		echo " without any authentication. Make sure your server is configured to accept emails coming from this IP."
+	fi
+else
+	echo "- Will try to deliver emails directly to the final server. Make sure your DNS is setup properly!"
+	postconf -# relayhost
+	postconf -# smtp_sasl_auth_enable
+	postconf -# smtp_sasl_password_maps
+	postconf -# smtp_sasl_security_options
+fi
+
+# Set up my networks to list only networks in the local loopback range
+
+if [ ! -z "$MYNETWORKS" ]; then
+	postconf -e mynetworks=$MYNETWORKS
+else
+	postconf -e "mynetworks=127.0.0.0/8,10.0.0.0/8,172.16.0.0/12"
+fi
